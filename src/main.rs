5e4fa6be50a79138b88f1167d55b0e6066b9f157
Neil McAlister <neil.mcalister@futurice.com>
2018-02-26 11:19:29 +0200

    Iterate through

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use std::env;
 use std::thread;
 
 extern crate wiringpi;
 
 fn main() {
-
-     let args: Vec<String> = env::args().collect();
-
-     let value = &args[1].parse::<i32>().unwrap();
     
     let pi = wiringpi::setup_gpio();
 
     let pin_4 = pi.soft_pwm_pin(4);
-    pin_4.pwm_write(*value);
+
+    for i in 0..100 {
+        pin_4.pwm_write(i);
+    }
+    
     thread::sleep_ms(1000);
 }
