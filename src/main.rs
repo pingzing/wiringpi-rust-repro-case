9c3bdd68f8748e7b37f53d0da5a78d5db670fdaf
Neil McAlister <neil.mcalister@futurice.com>
2018-02-26 11:29:42 +0200

    Light up all 3, twice

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,18 @@ fn main() {
     pins.push(pi.soft_pwm_pin(27));
     pins.push(pi.soft_pwm_pin(22));
 
+    //One at a time    
+    for pin in &pins {
+        pins[0].pwm_write(0);
+        pins[1].pwm_write(0);
+        pins[2].pwm_write(0);
+        for i in 0..100 {
+            pin.pwm_write(i);
+            thread::sleep_ms(100);        
+        }
+    }
+
+    // All 3 simultaneously
     for i in 0..100 {
         for pin in &pins {
             pin.pwm_write(i);
