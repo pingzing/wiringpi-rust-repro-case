36909e7a410c6ac12d9aff964430623e8ee21f6c
Neil McAlister <neil.mcalister@futurice.com>
2018-02-26 11:25:46 +0200

    Light up all 3

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ fn main() {
     
     let pi = wiringpi::setup_gpio();
 
-    let pin_4 = pi.soft_pwm_pin(4);
+    let mut pins: Vec<wiringpi::pin::SoftPwmPin<wiringpi::pin::Gpio>> = Vec::new();
+
+    pins.push(pi.soft_pwm_pin(17));
+    pins.push(pi.soft_pwm_pin(27));
+    pins.push(pi.soft_pwm_pin(22));
 
     for i in 0..100 {
-        pin_4.pwm_write(i);
+        for pin in &pins {
+            pin.pwm_write(i);
+        }        
         thread::sleep_ms(100);
     }
     
