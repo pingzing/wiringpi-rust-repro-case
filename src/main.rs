d702a440a2755fe4d73f14b81ad408518b64cc04
Neil McAlister <neil.mcalister@futurice.com>
2018-02-26 11:11:49 +0200

    Add command line args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::env;
+
 extern crate wiringpi;
 
 fn main() {
+
+     let args: Vec<String> = env::args().collect();
+
+     let value = &args[1].parse::<i32>().unwrap();
     
     let pi = wiringpi::setup_gpio();
 
     let pin_4 = pi.soft_pwm_pin(4);
-    pin_4.pwm_write(50);
+    pin_4.pwm_write(*value);
 }
